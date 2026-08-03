a68e63cd26be00f438df82dff815c7848d11fff8
naveiroinventor <jmnavegomez@gmail.com>
2026-08-03 18:36:46 +0200

    compiler

--- src/odix/Scriptorium/compiler.py
+++ src/odix/Scriptorium/compiler.py
@@ -1,2 +1,41 @@
-def build():
-    pass
+from __future__ import annotations
+
+from ..tabula.nodes import Document
+
+from .language import Language
+from .writer import Writer
+from .visitors.markup import MarkupVisitor
+
+
+class Compiler:
+    """Compiles a Tabula document into a markup language."""
+
+    def __init__(
+        self,
+        language: Language,
+    ) -> None:
+        """Initializes the compiler.
+
+        Args:
+            language: Target output language.
+        """
+
+        self._writer = Writer(language)
+        self._visitor = MarkupVisitor(
+            self._writer,
+        )
+
+    def compile(
+        self,
+        document: Document,
+    ) -> str:
+        """Compiles a document.
+
+        Args:
+            document: Document to compile.
+
+        Returns:
+            Generated markup.
+        """
+
+        return self._visitor.visit(document)
